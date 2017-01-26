be406925af43f60c2df95763e18724e1bec3b714
haotian <nimei@gmail.com>
2017-01-25 22:11:12 -0600

    added something in WillDelete

--- src/WillDelete.java
+++ src/WillDelete.java
@@ -1,7 +1,8 @@
-package PACKAGE_NAME;
-
 /**
  * Created by haotian on 1/25/17.
  */
 public class WillDelete {
+    public static void main(String[] args){
+        System.out.print("I will delete this soon");
+    }
 }
